561ada04df5c6f235da84990e912aa2d4ae877ce
sleepingkirby <sleepingkirby@gmail.com>
2024-09-28 13:43:47 +0800

    interm commit. Just some variables and css added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import { Cal } from './component/Cal.js'
 
 function App() {
+
+  const [curDate, setCurDate] = useState(new Date()); //defaults to today
+  const [dateRng, setDateRng] = useState({ 'start': null, 'end': null }); //date range selected
+
   return (
     <div className="App">
-      <Cal />
+      <Cal curDate={curDate} dateRng={dateRng} />
     </div>
   );
 }
--- src/component/Cal.js
+++ src/component/Cal.js
@@ -1,13 +1,17 @@
-//import React from 'react';
+import React, { useState } from 'react';
 import cn from './Cal.module.css'
 
-export const Cal = () => {
+export const Cal = ({ curDate = new Date(), dateRng = { 'start': null, 'end': null }}) => {
+  
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
 
-    return (
-        <div className={`${cn.Cal}`}>
-        Calendar
-        </div>
-    )
+
+  return (
+      <div className={`${cn.Cal}`}>
+      Calendar
+      </div>
+  )
 }
 
