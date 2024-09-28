2f84e8dd8372dfba27432f463e4a5e98711608db
sleepingkirby <sleepingkirby@gmail.com>
2024-09-28 14:19:07 +0800

    more css styles

--- src/component/Cal.js
+++ src/component/Cal.js
@@ -5,11 +5,17 @@ export const Cal = ({ curDate = new Date(), dateRng = { 'start': null, 'end': nu
   
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-
-
+  const today = new Date();
+  const mon = today.getMonth() + 1;
+  const year = today.getFullYear() + 1;
 
   return (
       <div className={`${cn.Cal}`}>
+        <div className={`${cn.CalMonNav}`}>
+          <div className={`${cn.CalMonNavArrow}`}>&lt;</div>
+          <div className={`${cn.CalMonNavDate}`}>{year}年{mon}月</div>
+          <div className={`${cn.CalMonNavArrow}`}>&gt;</div>
+        </div>
       Calendar
       </div>
   )
